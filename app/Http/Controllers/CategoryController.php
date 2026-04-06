0a0519595232c8ca90e094b8a74e5717d28d5b6e
mohamed <mohamedlfarh7@gmail.com>
2026-04-06 12:06:24 +0100

    create category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function show(Category $category)
+    {
+        $services = $category->services()->paginate(9);
+        return view('services.index', compact('services', 'category'));
+    }
 }
